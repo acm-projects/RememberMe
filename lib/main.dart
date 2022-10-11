5e2256bb21c2723746a9ba1042ac649362795029
Kar-8 <karameimei8@gmail.com>
2022-10-11 16:49:35 -0500

    Made "Screens" into "screens"

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'login-signup.dart';
 import 'Screens/MemorySelect.dart';
 
 void main() async {
