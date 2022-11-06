129bb5488535d5d1dbfe24f8ec54d12e6d1f17e0
Kar-8 <karameimei8@gmail.com>
2022-11-06 09:29:12 -0600

    Tried making the appbar on the profile look like the homescreen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -2,6 +2,97 @@ import 'package:flutter/material.dart';
 import 'package:rememberme/screens/login-signup.dart';
 import 'package:rememberme/services/authservice.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rememberme/widgets/roundedpage.dart';
+
+class RoundedPage extends StatelessWidget {
+  const RoundedPage({
+    super.key,
+    this.title,
+    this.floatingActionButton,
+    this.onRefresh,
+    this.appBarActions,
+    this.roundedMargin = 20,
+    this.bodyMargin = 40,
+    required this.child,
+  });
+
+  final String? title;
+  final Widget child;
+  final Widget? floatingActionButton;
+  final Future<void> Function()? onRefresh;
+  final List<Widget>? appBarActions;
+  final double roundedMargin;
+  final double bodyMargin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 0,
+        title: title != null ? Text(title!) : null,
+        actions: appBarActions,
+        leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.white,
+              size: 35,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }
+
+        ),
+      ),
+      floatingActionButton: floatingActionButton,
+      body: Scaffold(
+        body: Center(
+          child: onRefresh == null
+              ? _getBody(context)
+              : _getBodyWithRefesh(context),
+        ),
+      ),
+    );
+  }
+
+  Widget _getBodyWithRefesh(context) {
+    assert(onRefresh != null);
+    return RefreshIndicator(
+      onRefresh: onRefresh!,
+      child: _getBody(context),
+    );
+  }
+
+  Widget _getBody(BuildContext context) {
+    return ListView(
+      children: [
+        Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              color: Theme.of(context).primaryColor,
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height,
+              margin: EdgeInsets.only(top: roundedMargin),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: bodyMargin),
+              child: child,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
 
 
 class Profile extends StatefulWidget {
@@ -16,159 +107,134 @@ class _ProfileState extends State<Profile> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          //backgroundColor: const Color(0xFFFF8F54),
-          backgroundColor: Theme.of(context).primaryColorDark,
-          elevation: 0,
-          centerTitle: true,
-          title: const Text(
-              'My Profile',
-              style: TextStyle(
-                color: Colors.white,
-                //fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
-        ),
-        leading: IconButton(
-          icon: const Icon(
-          Icons.arrow_back,
-          color: Colors.white,
-          size: 40,
-        ),
-        onPressed: () {
-            Navigator.pop(context);
-          }
+    return
+      RoundedPage(
+        title: 'Profile',
 
-          ), // IconButton
-    ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              height: 730,
+              width: 500,
+              color: Colors.transparent,
 
-      body: Stack(
-        alignment: Alignment.topLeft,
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              color: Color(0xFFFF8F54),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  height: 730,
-                  width: 500,
-                  color: Colors.transparent,
-
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      color: Color.fromRGBO(251,250,250, 1),
-                      borderRadius: BorderRadius.only(topLeft: Radius.circular(47.0), topRight: Radius.circular(47.0), bottomLeft: Radius.zero, bottomRight: Radius.zero),
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Color.fromRGBO(251, 250, 250, 1),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(47.0),
+                      topRight: Radius.circular(47.0),
+                      bottomLeft: Radius.zero,
+                      bottomRight: Radius.zero),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    const Padding(
+                      padding: EdgeInsets.all(30),
+                      child: SizedBox(
+                        height: 170,
+                        width: 170,
+                        child: CircleAvatar(
+                          // add image to assets folder or get image from firebase and put here
+                          backgroundImage: AssetImage('assets/avatar.webp'),
+                        ),
+                      ),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget> [
+                    Column(
+                      children: [
                         const Padding(
-                          padding: EdgeInsets.all(30),
-                          child: SizedBox(
-                            height: 170,
-                            width: 170,
-                            child: CircleAvatar(
-                              // add image to assets folder or get image from firebase and put here
-                              backgroundImage: AssetImage('assets/avatar.webp'),
+                          padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                          child: Text(
+                            'Name:',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 28,
                             ),
+                            textAlign: TextAlign.left,
                           ),
                         ),
-                        Column(
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                              child: Text(
-                                'Name:',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 28,
-                                ),
-                                textAlign: TextAlign.left,
-                              ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                          child: Text(
+                            displayName(),
+                            style: TextStyle(
+                              color: Colors.black,
+                              //fontWeight: FontWeight.bold,
+                              fontSize: 27,
                             ),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                              child: Text(
-                                displayName(),
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  //fontWeight: FontWeight.bold,
-                                  fontSize: 27,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            const Padding(
-                              padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                              child: Text(
-                                'Email:',
-                                style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 28,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.fromLTRB(0, 10, 0, 60),
-                              child: Text(
-                                displayEmail(),
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  //fontWeight: FontWeight.bold,
-                                  fontSize: 27,
-                                ),
-                                textAlign: TextAlign.center,
-                              ),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 80,
-                          width: 300,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              auth.signOut();
-                              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginOptions()));
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color.fromRGBO(255,164,116, 1),
-                              shape: RoundedRectangleBorder( //to set border radius to button
-                                  borderRadius: BorderRadius.circular(20)
-                              ),
-                            ),
-                            child: const Text(
-                              'Sign Out',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 25,
-                              ),
-                            ),
+                            textAlign: TextAlign.center,
                           ),
                         ),
                       ],
                     ),
-                  ),
+                    Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(0, 40, 0, 0),
+                          child: Text(
+                            'Email:',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 28,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 10, 0, 60),
+                          child: Text(
+                            displayEmail(),
+                            style: const TextStyle(
+                              color: Colors.black,
+                              //fontWeight: FontWeight.bold,
+                              fontSize: 27,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 80,
+                      width: 300,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          auth.signOut();
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(builder: (context) =>
+                                  LoginOptions()));
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromRGBO(
+                              255, 164, 116, 1),
+                          shape: RoundedRectangleBorder( //to set border radius to button
+                              borderRadius: BorderRadius.circular(20)
+                          ),
+                        ),
+                        child: const Text(
+                          'Sign Out',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
   }
+
+
 
   String displayName() {
     String name = "";
